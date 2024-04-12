ee229f24a9c4053b039e706ec72c84cd45412364
Leighton Miguel <leim56@hawaii.rr.com>
2024-04-11 19:12:16 -1000

    Update settings.py

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('PROJECT_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'admin.8bituhm.org']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'www.8bituhm.org','8bituhm.org']
 
 
 # Application definition
