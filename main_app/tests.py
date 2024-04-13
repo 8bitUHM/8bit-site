5f050bd572e9547df196ab79e2838d7afa2ef1c6
Leighton Miguel <leim56@hawaii.rr.com>
2024-04-12 22:40:24 -1000

    Revert "member creation test cases"
    This reverts commit 16892f5298c67c059f152d13094974b59d598d19.

--- main_app/tests.py
+++ main_app/tests.py
@@ -1,14 +1,3 @@
 from django.test import TestCase
-from django.core.exceptions import ValidationError
-from main_app.models import Member
 
-class MemberModelTestCase(TestCase):
-    def test_create_member_without_name(self):
-        b = Member(team='business', role='Member')
-        b.save()
-        # with self.assertRaises(ValidationError):
-        #     b.save()
-            
-    def test_create_member_with_name(self):
-        member = Member.objects.create(name='John Doe', team='business', role='Member')
-        self.assertIsNotNone(member)
+# Create your tests here.
