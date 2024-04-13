16892f5298c67c059f152d13094974b59d598d19
Leighton Miguel <leim56@hawaii.rr.com>
2024-04-12 22:11:28 -1000

    member creation test cases

--- main_app/tests.py
+++ main_app/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+from main_app.models import Member
 
-# Create your tests here.
+class MemberModelTestCase(TestCase):
+    def test_create_member_without_name(self):
+        b = Member(team='business', role='Member')
+        b.save()
+        # with self.assertRaises(ValidationError):
+        #     b.save()
+            
+    def test_create_member_with_name(self):
+        member = Member.objects.create(name='John Doe', team='business', role='Member')
+        self.assertIsNotNone(member)
