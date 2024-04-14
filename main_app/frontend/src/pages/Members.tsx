23f7030103d026fc7f6faeb47f419b3ef813cd0e
Leighton Miguel <leim56@hawaii.rr.com>
2024-04-13 15:13:44 -1000

    members page template

--- main_app/frontend/src/pages/Members.tsx
+++ main_app/frontend/src/pages/Members.tsx
@@ -9,7 +9,218 @@ const Members = () => {
         className="container text-left my-5 my-md-1"
         style={{ paddingTop: 150 }}
       >
-        This site is still under construction.
+        <div className="mb-4">
+          <div className="row">
+            <div className="col-12 text-center">
+              <div className="section-title">
+                <h4 className="title ">Business Team</h4>
+                <p className="text-muted para-desc mx-auto mb-0">
+                  The driving force behind client acquisition and member
+                  engagement. With strategic prowess and a passion for
+                  connection, they fuel our organization's growth and community
+                  spirit.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-3 col-md-4 col-6 mt-4 pt-2">
+              <div className="team-list position-relative overflow-hidden shadow rounded">
+                <img
+                  src="https://bootdey.com/img/Content/avatar/avatar7.png"
+                  className="img-fluid float-left"
+                  alt=""
+                />
+                <div className="content float-right p-4">
+                  <h5 className="title mb-0">Martin Dongre</h5>
+                  <small className="text-muted">Founder</small>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4 col-6 mt-4 pt-2">
+              <div className="team-list position-relative overflow-hidden shadow rounded">
+                <img
+                  src="https://bootdey.com/img/Content/avatar/avatar6.png"
+                  className="img-fluid "
+                  alt=""
+                />
+                <div className="content float-right p-4">
+                  <h5 className="title mb-0">Lisa Marvel</h5>
+                  <small className="text-muted">C.E.O.</small>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4 col-6 mt-4 pt-2">
+              <div className="team-list position-relative overflow-hidden shadow rounded">
+                <img
+                  src="https://bootdey.com/img/Content/avatar/avatar6.png"
+                  className="img-fluid "
+                  alt=""
+                />
+                <div className="content float-right p-4">
+                  <h5 className="title mb-0">Lisa Marvel</h5>
+                  <small className="text-muted">C.E.O.</small>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4 col-6 mt-4 pt-2">
+              <div className="team-list position-relative overflow-hidden shadow rounded">
+                <img
+                  src="https://bootdey.com/img/Content/avatar/avatar6.png"
+                  className="img-fluid "
+                  alt=""
+                />
+                <div className="content float-right p-4">
+                  <h5 className="title mb-0">Lisa Marvel</h5>
+                  <small className="text-muted">C.E.O.</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr></hr>
+        <div className="mb-4">
+          <div className="row">
+            <div className="col-12 text-center">
+              <div className="section-title">
+                <h4 className="title ">Design Team</h4>
+                <p className="text-muted para-desc mx-auto mb-0">
+                  The creative visionaries shaping our brand's identity. With an
+                  eye for aesthetics and a flair for innovation, they craft
+                  captivating visuals that elevate our message and captivate our
+                  audience.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-3 col-md-4 col-6 mt-4 pt-2">
+              <div className="team-list position-relative overflow-hidden shadow rounded">
+                <img
+                  src="https://bootdey.com/img/Content/avatar/avatar7.png"
+                  className="img-fluid float-left"
+                  alt=""
+                />
+                <div className="content float-right p-4">
+                  <h5 className="title mb-0">Martin Dongre</h5>
+                  <small className="text-muted">Founder</small>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4 col-6 mt-4 pt-2">
+              <div className="team-list position-relative overflow-hidden shadow rounded">
+                <img
+                  src="https://bootdey.com/img/Content/avatar/avatar6.png"
+                  className="img-fluid "
+                  alt=""
+                />
+                <div className="content float-right p-4">
+                  <h5 className="title mb-0">Lisa Marvel</h5>
+                  <small className="text-muted">C.E.O.</small>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4 col-6 mt-4 pt-2">
+              <div className="team-list position-relative overflow-hidden shadow rounded">
+                <img
+                  src="https://bootdey.com/img/Content/avatar/avatar6.png"
+                  className="img-fluid "
+                  alt=""
+                />
+                <div className="content float-right p-4">
+                  <h5 className="title mb-0">Lisa Marvel</h5>
+                  <small className="text-muted">C.E.O.</small>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4 col-6 mt-4 pt-2">
+              <div className="team-list position-relative overflow-hidden shadow rounded">
+                <img
+                  src="https://bootdey.com/img/Content/avatar/avatar6.png"
+                  className="img-fluid "
+                  alt=""
+                />
+                <div className="content float-right p-4">
+                  <h5 className="title mb-0">Lisa Marvel</h5>
+                  <small className="text-muted">C.E.O.</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr></hr>
+        <div className="mb-4">
+          <div className="row">
+            <div className="col-12 text-center">
+              <div className="section-title">
+                <h4 className="title ">Software Team</h4>
+                <p className="text-muted para-desc mx-auto mb-0">
+                  Our Software Team pioneers innovation, transforming ideas into
+                  robust digital solutions. With expertise in coding and
+                  problem-solving, they craft seamless experiences that propel
+                  our organization forward.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-3 col-md-4 col-6 mt-4 pt-2">
+              <div className="team-list position-relative overflow-hidden shadow rounded">
+                <img
+                  src="https://bootdey.com/img/Content/avatar/avatar7.png"
+                  className="img-fluid float-left"
+                  alt=""
+                />
+                <div className="content float-right p-4">
+                  <h5 className="title mb-0">Martin Dongre</h5>
+                  <small className="text-muted">Founder</small>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4 col-6 mt-4 pt-2">
+              <div className="team-list position-relative overflow-hidden shadow rounded">
+                <img
+                  src="https://bootdey.com/img/Content/avatar/avatar6.png"
+                  className="img-fluid "
+                  alt=""
+                />
+                <div className="content float-right p-4">
+                  <h5 className="title mb-0">Lisa Marvel</h5>
+                  <small className="text-muted">C.E.O.</small>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4 col-6 mt-4 pt-2">
+              <div className="team-list position-relative overflow-hidden shadow rounded">
+                <img
+                  src="https://bootdey.com/img/Content/avatar/avatar6.png"
+                  className="img-fluid "
+                  alt=""
+                />
+                <div className="content float-right p-4">
+                  <h5 className="title mb-0">Lisa Marvel</h5>
+                  <small className="text-muted">C.E.O.</small>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-4 col-6 mt-4 pt-2">
+              <div className="team-list position-relative overflow-hidden shadow rounded">
+                <img
+                  src="https://bootdey.com/img/Content/avatar/avatar6.png"
+                  className="img-fluid "
+                  alt=""
+                />
+                <div className="content float-right p-4">
+                  <h5 className="title mb-0">Lisa Marvel</h5>
+                  <small className="text-muted">C.E.O.</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
