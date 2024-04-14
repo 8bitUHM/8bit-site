cc27fa3c69bd51c361073023dd0dd833f6286138
Leighton Miguel <leim56@hawaii.rr.com>
2024-04-13 16:39:02 -1000

    Update views.py

--- api/views.py
+++ api/views.py
@@ -2,13 +2,12 @@ from .models import *
 from .serializers import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
-# Create your views here.
-
+from django.db.models.functions import Lower
 
 class GetMembers(APIView):
     def get(self, request):
         leaders = Member.objects.filter(is_leader=True).prefetch_related("social_medias")
-        non_leaders = Member.objects.filter(is_leader=False).prefetch_related("social_medias")
+        non_leaders = Member.objects.filter(is_leader=False).prefetch_related("social_medias").order_by(Lower('name'))
         
         queryset = list(leaders) + list(non_leaders)
         
