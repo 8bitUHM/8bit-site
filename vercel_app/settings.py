b22da75ebc315d77b8a6993c86a2f345f5d5c2a7
Leighton Miguel <leim56@hawaii.rr.com>
2024-04-14 15:15:08 -1000

    Update settings.py

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('PROJECT_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'www.8bituhm.org','8bituhm.org']
 
