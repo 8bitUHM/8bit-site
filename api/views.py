3ee444e1be36dd127d855f93b7e2f351309e615d
Leighton Miguel <leim56@hawaii.rr.com>
2024-04-18 12:11:40 -1000

    Update views.py

--- api/views.py
+++ api/views.py
@@ -6,10 +6,11 @@ from django.db.models.functions import Lower
 
 class GetMembers(APIView):
     def get(self, request):
-        leaders = Member.objects.filter(is_leader=True).prefetch_related("social_medias")
-        non_leaders = Member.objects.filter(is_leader=False).prefetch_related("social_medias").order_by(Lower('name'))
+        leaders = Member.objects.filter(is_leader=True).prefetch_related("social_medias").order_by('name')
+        non_leaders = Member.objects.filter(is_leader=False).exclude(role="Member, Trainee Developer").prefetch_related("social_medias").order_by('name')
+        trainee_developers = Member.objects.filter(is_leader=False, role="Member, Trainee Developer").prefetch_related("social_medias").order_by('name')
         
-        queryset = list(leaders) + list(non_leaders)
+        queryset = list(leaders) + list(non_leaders) + list(trainee_developers)
         
         serializer = MemberSerializer(queryset, many=True, context={'request': request})
         return Response(serializer.data)
