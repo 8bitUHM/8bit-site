3e296140e1d5b38d0930d2ed10c32f76656f032f
Leighton Miguel <leim56@hawaii.rr.com>
2024-04-18 12:21:37 -1000

    Update views.py

--- api/views.py
+++ api/views.py
@@ -3,14 +3,18 @@ from .serializers import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.db.models.functions import Lower
+from django.db.models import Case, When, Value, IntegerField
 
 class GetMembers(APIView):
     def get(self, request):
-        leaders = Member.objects.filter(is_leader=True).prefetch_related("social_medias").order_by('name')
-        non_leaders = Member.objects.filter(is_leader=False).exclude(role="Member, Trainee Developer").prefetch_related("social_medias").order_by('name')
-        trainee_developers = Member.objects.filter(is_leader=False, role="Member, Trainee Developer").prefetch_related("social_medias").order_by('name')
-        
-        queryset = list(leaders) + list(non_leaders) + list(trainee_developers)
-        
+        queryset = Member.objects.annotate(
+            custom_order=Case(
+                When(is_leader=True, then=Value(1)),
+                When(role="Member, Trainee Developer", then=Value(3)),
+                default=Value(2),
+                output_field=IntegerField(),
+            )
+        ).order_by('custom_order', 'name')
+
         serializer = MemberSerializer(queryset, many=True, context={'request': request})
         return Response(serializer.data)
