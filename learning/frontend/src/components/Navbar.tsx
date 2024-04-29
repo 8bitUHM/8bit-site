0a9d6a2ad5b35725d2ba33d8e051ad0c1ef97b54
Leighton Miguel <leim56@hawaii.rr.com>
2024-04-28 21:15:04 -1000

    footer and navbar

---
+++ learning/frontend/src/components/Navbar.tsx
@@ -0,0 +1,78 @@
+import * as React from "react";
+import { FC } from "react";
+
+const Navbar: FC = () => {
+  return (
+    <>
+      <nav
+        className="navbar navbar-expand-md navbar-light fixed-top"
+        style={{ backgroundColor: "#3d8581" }}
+      >
+        <div className="container justify-content-md-between">
+          <a href="./" className="ms-md-2 d-md-block d-none">
+            <img src="/static/main_app/assets/8bit-long-logo.png" height={35} />
+          </a>
+          <button
+            className="navbar-toggler"
+            data-bs-toggle="collapse"
+            data-bs-target="#nav"
+            aria-controls="nav"
+            aria-label="Expand Navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="nav">
+            <ul className="navbar-nav navbar-right">
+              <li className="nav-item">
+                <a href="./" className="nav-link text-white">
+                  Lessons
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a href="/" className="nav-link text-white">
+                  Main Site
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <span className="navbar-text fw-bold d-md-block">
+            <div className="dropdown">
+              <button
+                className="btn  btn-outline-light"
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  fill="currentColor"
+                  className="bi bi-person-circle btn-outline-light "
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                  <path
+                    fill-rule="evenodd"
+                    d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+                  />
+                </svg>
+              </button>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Logout
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </span>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
--- learning/frontend/src/pages/Index.tsx
+++ learning/frontend/src/pages/Index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Index = () => {
   const [pageReady, setPageReady] = useState<boolean>(false);
@@ -9,29 +11,44 @@ const Index = () => {
   useEffect(() => {
     try {
       const lessons = (window as any).data as any;
-      lessons.forEach((lesson:any)=>{
-        console.log(lesson)
-      })
-      
+      lessons.forEach((lesson: any) => {
+        console.log(lesson);
+      });
+
       setCanMap(true);
       setPageReady(true);
     } catch (e: any) {
       setPageReady(true);
     }
   }, []);
-  return pageReady ? (
+  return (
     <>
-      <div className="container text-left">
-        Learning hii
+      <Navbar />
+      <div className="container text-left" style={{ marginTop: 80 }}>
+        {pageReady ? (
+          <>
+            {canMap ? (
+              <>
+                <div>Leanring page haha</div>
+              </>
+            ) : (
+              <div style={{ marginBottom: 700 }}>
+                Uh oh! Something went wrong with our request for data. Please
+                refresh and try again!
+              </div>
+            )}
+          </>
+        ) : (
+          <div
+            className="d-flex justify-content-center"
+            style={{ marginBottom: 700 }}
+          >
+            <div className="spinner-border my-5"></div>
+          </div>
+        )}
       </div>
+      <Footer />
     </>
-  ): (
-    <div
-      className="d-flex justify-content-center"
-      style={{ marginBottom: 700 }}
-    >
-      <div className="spinner-border my-5"></div>
-    </div>
   );
 };
 
