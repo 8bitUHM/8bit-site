be9d615b6f01eb5a36f878196faa80d5c48eeac3
Leighton Miguel <leim56@hawaii.rr.com>
2024-08-30 01:04:41 -1000

    Update projects mapping function

--- main_app/frontend/src/pages/Projects.tsx
+++ main_app/frontend/src/pages/Projects.tsx
@@ -37,67 +37,68 @@ const Projects = () => {
   }, []);
 
   const mapProjects = () => {
-    if (projectData.length > 0) {
-      return projectData.map((project: Project, index: number) => (
-        <div
-          key={index}
-          className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 mt-4 pt-2"
-        >
-          <div className="team-list position-relative overflow-hidden shadow rounded">
-            {project.image != null ? (
-              <LoadingImage
-                imageUri={project.image}
-                className="img-fluid float-left"
-              />
-            ) : (
-              <LoadingImage
-                imageUri={"/static/main_app/assets/default-member.png"}
-                className="img-fluid float-left"
-              />
-            )}
-            <div className="content float-right p-3">
-              <h5 className="title mb-0">{project.name}</h5>
-              <small className="text-muted">{project.description}</small>
-              <br></br>
-              {project.tags.map((tag, index) => {
-                return (
-                  <span key={index} className={`badge bg-primary me-1`}>
-                    {tag.tag_name}
-                  </span>
-                );
-              })}
-              <br></br>
-              <a
-                href={project.github_link}
-                target="_blank"
-                className="btn btn-dark me-1 mt-2"
-              >
-                Repository{" "}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="13"
-                  height="13"
-                  fill="currentColor"
-                  className="bi bi-box-arrow-up-right"
-                  viewBox="0 0 16 16"
-                  style={{ verticalAlign: "baseline" }}
+    return projectData.length > 0 ? (
+      <>
+        {projectData.map((project: Project, index: number) => (
+          <div
+            key={index}
+            className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 mt-4 pt-2"
+          >
+            <div className="team-list position-relative overflow-hidden shadow rounded">
+              {project.image != null ? (
+                <LoadingImage
+                  imageUri={project.image}
+                  className="img-fluid float-left"
+                />
+              ) : (
+                <LoadingImage
+                  imageUri={"/static/main_app/assets/default-member.png"}
+                  className="img-fluid float-left"
+                />
+              )}
+              <div className="content float-right p-3">
+                <h5 className="title mb-0">{project.name}</h5>
+                <small className="text-muted">{project.description}</small>
+                <br></br>
+                {project.tags.map((tag, index) => {
+                  return (
+                    <span key={index} className={`badge bg-primary me-1`}>
+                      {tag.tag_name}
+                    </span>
+                  );
+                })}
+                <br></br>
+                <a
+                  href={project.github_link}
+                  target="_blank"
+                  className="btn btn-dark me-1 mt-2"
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"
-                  />
-                  <path
-                    fill-rule="evenodd"
-                    d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"
-                  />
-                </svg>
-              </a>
+                  Repository{" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="13"
+                    height="13"
+                    fill="currentColor"
+                    className="bi bi-box-arrow-up-right"
+                    viewBox="0 0 16 16"
+                    style={{ verticalAlign: "baseline" }}
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"
+                    />
+                    <path
+                      fill-rule="evenodd"
+                      d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"
+                    />
+                  </svg>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      ));
-    }
-    return (
+        ))}
+      </>
+    ) : (
       <>
         <div
           style={{ marginBottom: 400 }}
