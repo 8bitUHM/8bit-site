b9fb6df7cec79254249d563cdda06cae17f84c19
Leighton Miguel <leim56@hawaii.rr.com>
2024-10-15 18:17:14 -1000

    Update Footer.tsx

--- main_app/frontend/src/compiled-components/Footer.tsx
+++ main_app/frontend/src/compiled-components/Footer.tsx
@@ -5,108 +5,76 @@ import { initFlowbite } from "flowbite";
 import "../styles/styles.css";
 
 const Footer: FC = () => {
+  React.useEffect(() => {
+    initFlowbite();
+  }, []);
+
   return (
     <>
-      <div className=" mt-5" style={{ position: "absolute", width: "100%" }}>
-        <section>
-          <footer
-            className="text-center text-white"
-            style={{ backgroundColor: "#25524f" }}
-          >
-            <div className="container py-3">
-              <div className="pb-3">
-                <a
-                  href="#"
-                  style={{ textDecoration: "none" }}
-                  className="text-white"
+      <hr className="w-6/12 mx-auto h-px  bg-gray-200 border-0 dark:bg-gray-700"></hr>
+      <footer className="w-full py-3 bg-white">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="max-w-3xl mx-auto">
+            
+            <div className="text-lg flex items-center justify-center flex-col gap-3 md:flex-row md:gap-10 transition-all duration-500  mb-1 py-2 ">
+              <div className="flex items-center text-gray-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="size-5"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    fill="currentColor"
-                    className="bi bi-arrow-up-circle"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"
-                    />
-                  </svg>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+
+                <a
+                  className="mx-1 text-base hover:underline"
+                  target="_blank"
+                  href="/admin"
+                >
+                  Member Login
                 </a>
               </div>
-              <div className="d-flex justify-content-center align-items-center">
-                <div className="px-1">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-person-circle"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                    <path
-                      fillRule="evenodd"
-                      d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
-                    />
-                  </svg>{" "}
-                  <a className="text-white" href="/admin">
-                    Member Login
-                  </a>
-                </div>
 
-                <div className="px-1">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-people"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4" />
-                  </svg>{" "}
-                  <a className="text-white" href="/admin">
-                    Client Login
-                  </a>
-                </div>
-              </div>
-              <div className="d-flex justify-content-center align-items-center pt-2">
-                <div>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-github"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
-                  </svg>{" "}
-                  <a
-                    className="text-white"
-                    target="_blank"
-                    href="https://github.com/8bitUHM/8bit-site"
-                  >
-                    Website Source Code
-                  </a>
-                </div>
+              <div className="flex items-center text-gray-500">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="15"
+                  height="15"
+                  fill="currentColor"
+                  className="bi bi-github"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8" />
+                </svg>
+                <a
+                  className="mx-1 text-base hover:underline"
+                  target="_blank"
+                  href="https://github.com/8bitUHM/8bit-site"
+                >
+                  Website Source Code
+                </a>
               </div>
             </div>
-            <div
-              className="text-center p-3"
-              style={{ backgroundColor: "#1b3d3b" }}
-            >
-              © 2024 Copyright{" "}
-              <a className="text-white" href="https://www.8bituhm.org">
-                8bituhm.org
-              </a>
-            </div>
-          </footer>
-        </section>
-      </div>
+
+            <span className="text-sm text-gray-500 text-center block">
+              ©
+              <a href="https://8bituhm.org/" target="_blank">
+                8bitUHM
+              </a>{" "}
+              2024, All rights reserved.
+            </span>
+          </div>
+        </div>
+      </footer>
     </>
+    
   );
 };
 
