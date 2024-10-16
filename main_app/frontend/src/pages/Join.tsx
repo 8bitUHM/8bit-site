5109a4ec60718ace4d23e7e32b0b5c0f948754af
Leighton Miguel <leim56@hawaii.rr.com>
2024-10-16 11:32:38 -1000

    New Join page

--- main_app/frontend/src/pages/Join.tsx
+++ main_app/frontend/src/pages/Join.tsx
@@ -7,48 +7,45 @@ const Join = () => {
   return (
     <>
       <div
-        className="container text-left my-5 my-md-1"
-        style={{ paddingTop: 150, paddingBottom: 300 }}
+        className="container sm:mx-auto px-5 max-w-screen-xl"
+        style={{ paddingTop: 125 }}
         data-aos="fade-up"
         data-aos-duration="1500"
       >
-        <div className="row">
-          <div className="col">
-            <p className="lead fs-4 text-secondary mb-3">
-              Thank you for your interest in 8bit!
+        <section className="bg-white dark:bg-gray-900">
+          <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+              Join{" "}
+              <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+                8bit
+              </span>
+            </h1>
+            <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+              All information for joining the 8bit Software Team can be found in
+              our Discord read me channel.
             </p>
-            <p>
-              8bit, a student group from the University of Hawaii at Manoa, is
-              passionate about software development. We specialize in full-stack
-              website creation, working closely with organizations to bring
-              their digital ideas to life. With dedicated teams in software,
-              design, and business, we offer a collaborative experience that
-              promotes growth across disciplines.
-            </p>
-            <p className="lead fs-4 text-secondary mb-1">Mission:</p>
-            <p className="mb-4">
-              "Empowering innovation and digital transformation, 8bit is
-              committed to leveraging our expertise in software development to
-              deliver tailored solutions that drive impact and success for our
-              organizations."
-            </p>
-            <p className="lead fs-4 text-secondary mb-1">Purpose:</p>
-            <p className="mb-4">
-              "Our purpose is to cultivate a collaborative environment where
-              students can hone their skills in software development while
-              creating meaningful digital experiences that address real-world
-              challenges."
-            </p>
-            <h4>
-              All of the information to join 8bit's software team can be found
-              in our{" "}
-              <a href="https://discord.gg/T7Eu75fpAf" target="_blank">
-                Discord
-              </a>{" "}
-              read-me channel.
-            </h4>
+            <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+              <a
+                href="https://discord.gg/T7Eu75fpAf"
+                target="_blank"
+                className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+              >
+                <svg
+                  className="w-6 h-6 mr-2 text-black"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M18.942 5.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.586 11.586 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3 17.392 17.392 0 0 0-2.868 11.662 15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.638 10.638 0 0 1-1.706-.83c.143-.106.283-.217.418-.331a11.664 11.664 0 0 0 10.118 0c.137.114.277.225.418.331-.544.328-1.116.606-1.71.832a12.58 12.58 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM8.678 14.813a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.929 1.929 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
+                </svg>
+                Our Discord
+              </a>
+            </div>
           </div>
-        </div>
+        </section>
       </div>
     </>
   );
