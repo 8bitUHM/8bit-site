d7f022bf26d314980632fba342c28d7a49fa0686
Leighton Miguel <leim56@hawaii.rr.com>
2024-10-16 18:06:40 -1000

    Learning navbar

--- learning/frontend/src/compiled-components/Navbar.tsx
+++ learning/frontend/src/compiled-components/Navbar.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import { FC } from "react";
 import { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
+import "../styles/styles.css";
+import { initFlowbite } from "flowbite";
 
 const Navbar: FC = () => {
   const [username, setUsername] = useState("");
@@ -13,62 +15,70 @@ const Navbar: FC = () => {
     } catch (e: any) {
       console.error(e);
     }
+    initFlowbite();
   }, []);
 
   return (
     <>
-      <nav
-        className="navbar navbar-expand-md navbar-light fixed-top"
-        style={{ backgroundColor: "#3d8581" }}
-      >
-        <div className="container justify-content-md-between">
-          <a href="https://8bituhm.org" className="ms-md-2 d-md-block d-none">
-            <img src="/static/main_app/assets/8bit-long-logo.png" height={35} />
+      <nav className="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+          <a
+            href="/learning"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <span className="self-center text-2xl font-bold text-white bg-gradient-to-r to-emerald-600 from-sky-600 p-2 rounded-lg shadow-lg">
+              8bit Learning
+            </span>
           </a>
           <button
-            className="navbar-toggler"
-            data-bs-toggle="collapse"
-            data-bs-target="#nav"
-            aria-controls="nav"
-            aria-label="Expand Navigation"
+            data-collapse-toggle="mega-menu-full"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="mega-menu-full"
+            aria-expanded="false"
           >
-            <span className="navbar-toggler-icon" />
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
           </button>
-          <div className="collapse navbar-collapse" id="nav">
-            <ul className="navbar-nav navbar-right">
-              <li className="nav-item">
-                <a href="/learning/lessons" className="nav-link text-white">
+          <div
+            id="mega-menu-full"
+            className="items-center justify-between font-medium hidden w-full md:flex md:w-auto md:order-1"
+          >
+            <ul className="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <a
+                  href="/learning"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal-700 md:p-0 dark:text-white md:dark:hover:text-teal-500 dark:hover:bg-gray-700 dark:hover:text-teal-500 md:dark:hover:bg-transparent dark:border-gray-700"
+                  aria-current="page"
+                >
                   Lessons
                 </a>
               </li>
 
-              <li className="nav-item">
-                <a href="/" className="nav-link text-white">
+              <li>
+                <a
+                  href="/"
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal-700 md:p-0 dark:text-white md:dark:hover:text-teal-500 dark:hover:bg-gray-700 dark:hover:text-teal-500 md:dark:hover:bg-transparent dark:border-gray-700"
+                >
                   Main Site
                 </a>
               </li>
             </ul>
           </div>
-
-          <span className="navbar-text fw-bold d-md-block">
-            <div className="dropdown">
-              <button
-                className="btn dropdown-toggle btn-outline-light d-inline"
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                {username}
-              </button>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="/learning/logout">
-                    Logout
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </span>
         </div>
       </nav>
     </>
