bc452fe7c0dc94e199060f80541da955addd556e
Leighton Miguel <leim56@hawaii.rr.com>
2024-10-16 14:11:11 -1000

    Remove learning portal auth

--- learning/views.py
+++ learning/views.py
@@ -2,26 +2,11 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import *
 import json
 from .serializers import LessonSerializer, SectionSerializer
-from django.contrib.auth.decorators import user_passes_test
-from django.contrib.auth.views import LoginView, LogoutView
-from .forms import LoginForm
-from django.urls import reverse_lazy
-from django.contrib import messages
-from .forms import CustomUserCreationForm
 
-def staff_or_group_required(group_name=None):
-  def check_user(user):
-    if user.is_staff:
-      return True
-    if group_name:
-      return user.groups.filter(name=group_name).exists()
-    return False
 
-  actual_decorator = user_passes_test(check_user, login_url='/learning/login/')
-  return actual_decorator
 
 # Create your views here.
-@staff_or_group_required('Learning')
+
 def index(request):
   username = request.user.username
   queryset = Lesson.objects.all()
@@ -29,7 +14,6 @@ def index(request):
   serialized_json_data = json.dumps(serializer.data)
   return render(request,'learning/pages/home.html',{"lessons":serialized_json_data,"username":username})
 
-@staff_or_group_required('Learning')
 def lesson(request,slug,page):
   username = request.user.username
   lesson = get_object_or_404(Lesson, slug=slug)
@@ -55,31 +39,3 @@ def quiz(request,slug):
   
   
   return render(request, 'learning/pages/quiz.html', {"lesson" : serialized_lesson_data,"username":username })
-
-class LearningLoginView(LoginView):
-  template_name = "learning/pages/login.html"
-  authentication_form = LoginForm
-  
-  def form_valid(self, form):
-    user = form.get_user()
-    if not user.is_staff and not user.groups.filter(name='Learning').exists():
-      messages.error(self.request, "Please enter a correct username and password. Note that both fields may be case-sensitive.")
-      return self.form_invalid(form)
-    return super().form_valid(form)
-
-  def get_success_url(self):
-    return reverse_lazy('lessons_view')
-  
-class LearningLogoutView(LogoutView):
-  next_page = '/'
-
-
-def signup(request):
-  if request.method == 'POST':
-    form = CustomUserCreationForm(request.POST)
-    if form.is_valid():
-      form.save()
-      return redirect('learning_login')  
-  else:
-    form = CustomUserCreationForm()
-  return render(request, 'learning/pages/signup.html', {'form': form})
