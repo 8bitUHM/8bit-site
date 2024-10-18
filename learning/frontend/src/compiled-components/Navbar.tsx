79dba25af0c4f6bf05aa5e47aafa93a097ce56be
Leighton Miguel <leim56@hawaii.rr.com>
2024-10-17 14:20:00 -1000

    Learning navbar

--- learning/frontend/src/compiled-components/Navbar.tsx
+++ learning/frontend/src/compiled-components/Navbar.tsx
@@ -12,20 +12,20 @@ const Navbar: FC = () => {
 
   return (
     <>
-      <nav className="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b">
+      <nav className="bg-teal-600 border-gray-200 dark:border-gray-600 dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
           <a
             href="/learning"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <span className="self-center text-2xl font-bold text-white bg-gradient-to-r to-emerald-600 from-sky-600 p-2 rounded-lg shadow-lg">
+            <span className="self-center text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r to-emerald-300 from-sky-300  ">
               8bit Learning
             </span>
           </a>
           <button
             data-collapse-toggle="mega-menu-full"
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg md:hidden hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="mega-menu-full"
             aria-expanded="false"
           >
@@ -50,11 +50,11 @@ const Navbar: FC = () => {
             id="mega-menu-full"
             className="items-center justify-between font-medium hidden w-full md:flex md:w-auto md:order-1"
           >
-            <ul className="flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="flex flex-col p-4 md:p-0 mt-4 border  rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <a
                   href="/learning"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal-700 md:p-0 dark:text-white md:dark:hover:text-teal-500 dark:hover:bg-gray-700 dark:hover:text-teal-500 md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 px-3 text-white rounded hover:bg-teal-400 md:hover:bg-transparent md:hover:text-teal-300 md:p-0 dark:text-white md:dark:hover:text-teal-500 dark:hover:bg-gray-700 dark:hover:text-teal-500 md:dark:hover:bg-transparent dark:border-gray-700"
                   aria-current="page"
                 >
                   Lessons
@@ -64,7 +64,7 @@ const Navbar: FC = () => {
               <li>
                 <a
                   href="/"
-                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal-700 md:p-0 dark:text-white md:dark:hover:text-teal-500 dark:hover:bg-gray-700 dark:hover:text-teal-500 md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="block py-2 px-3 text-white rounded hover:bg-teal-400 md:hover:bg-transparent md:hover:text-teal-300 md:p-0 dark:text-white md:dark:hover:text-teal-500 dark:hover:bg-gray-700 dark:hover:text-teal-500 md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Main Site
                 </a>
